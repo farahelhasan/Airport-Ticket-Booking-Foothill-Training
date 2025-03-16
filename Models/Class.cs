bfe0ad9469c0469cfe2d9cc8117ba54b69f2e45d
Farah Elhasan <farahelhasan32@gmail.com>
2025-03-16 14:23:42 +0200

    Create Class model

---
+++ Models/Class.cs
@@ -0,0 +1,18 @@
+﻿using Airport_Ticket_Booking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Models
+{
+    class Class
+    {
+        public int FlightID { set; get; }
+        public double Price { set; get; }
+        public string ClassType { set; get; }
+        public int SeatsAvailable { set; get; }
+
+    }
+}
