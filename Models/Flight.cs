6fd89c9e4d77a1ebf5c40e615fa3b437b3e236d7
Farah Elhasan <farahelhasan32@gmail.com>
2025-03-16 14:18:14 +0200

    add properties to Flight model

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Airport_Ticket_Booking.Models
 {
+
     class Flight
     {
+        public int FlightID { set; get; }
+        public string DepartureCountry { set; get; }
+        public string DestinationCountry { set; get; }
+        public string DepartureAirport { set; get; }
+        public string ArrivalAirport { set; get; }
+        public DateTime DepartureDate { set; get; }
+
+        public double Price { set; get; }
+
     }
 }
