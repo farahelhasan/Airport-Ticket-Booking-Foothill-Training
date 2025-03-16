3cead9622df3b03d1c5b198a591dc60c2ce3dab6
Farah Elhasan <farahelhasan32@gmail.com>
2025-03-16 14:28:29 +0200

    Create Booking model

---
+++ Models/Booking.cs
@@ -0,0 +1,20 @@
+﻿using Airport_Ticket_Booking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Models
+{
+    class Booking
+    {
+        public int BookingID { set; get; }
+
+        public int FlightID { set; get; }
+        public int UserID { set; get; }
+
+        public string Class { set; get; }
+       
+    }
+}
