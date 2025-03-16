e2461d65692431ded26b27bf9c26d44904729cf8
Farah Elhasan <farahelhasan32@gmail.com>
2025-03-16 13:44:28 +0200

    Create User model

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Models
+{
+    class User
+    {
+        public string Username { set; get; }
+        public string Password { set; get; }
+    }
+}
