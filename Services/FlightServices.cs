7481c1c9d6695408a46ad71fcae3ed38a43b990f
Farah Elhasan <farahelhasan32@gmail.com>
2025-03-17 16:50:17 +0200

    Add method to search flights by DestinationCountry, DestinationCountry, Date

---
+++ Services/FlightServices.cs
@@ -0,0 +1,24 @@
+﻿using Airport_Ticket_Booking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Services
+{
+    class FlightServices
+    {
+        public static List<Flight> SearchFlights(string DepartureCountry, string DestinationCountry, string DepartureDate)
+        {
+            List <Flight> flights = FileHandler.ReadFlights();
+            List<Flight> result = (from flight in flights
+                                   where flight.DepartureCountry.Equals(DepartureCountry, StringComparison.OrdinalIgnoreCase)
+                                   && flight.DestinationCountry.Equals(DestinationCountry, StringComparison.OrdinalIgnoreCase)
+                                   && flight.DepartureDate.Equals(DateTime.Parse(DepartureDate))
+                                   && flight.DepartureDate >= DateTime.Now
+                                   select flight).ToList();
+            return result;
+        }
+    }
+}
