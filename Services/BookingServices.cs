cf8575fc2501d50b6179614bec26e692a048d496
Farah Elhasan <farahelhasan32@gmail.com>
2025-03-19 13:07:37 +0200

    Add GetSpesificBooking method to show spesific booking

--- Services/BookingServices.cs
+++ Services/BookingServices.cs
@@ -11,21 +11,28 @@ namespace Airport_Ticket_Booking.Services
     class BookingServices
     {
     
-        public static Booking BookFlight(int flightId, string classType, int userId)
+        public static void BookFlight(int flightId, string classType, int userId)
         {
             // git the price for the class 
-            double proice = FlightServices.GetPrice(flightId, classType);
+            double price = FlightServices.GetPrice(flightId, classType);
 
             Flight flightToBook = FlightServices.GetFlight(flightId);
            
-            if(flightToBook != null && proice > 0)
+            if(flightToBook != null && price > 0)
             {
-                Booking booking = new Booking { BookingID = 1, FlightID = flightToBook.FlightID, Class = classType, UserID = userId, Price = proice };
+                Booking booking = new Booking { BookingID = 1, FlightID = flightToBook.FlightID, Class = classType, UserID = userId, Price = price };
                 FileHandler.SaveBooking(booking);
                 FlightServices.DecreaseSeatsAvailable(flightId, classType);
-                return booking;
+              
+                Console.WriteLine("Booking Successful!");
+                Console.WriteLine(booking);
+
             }
-            return null;
+            else
+            {
+                Console.WriteLine("Enter vaild FlightId...");
+
+            }
         }
 
         public static void GetUserBooking(int userId)
@@ -48,6 +55,26 @@ namespace Airport_Ticket_Booking.Services
                 }
             }
         }
-       
+
+        public static void GetSpesificBooking(int bookingId)
+        {
+            List<Booking> bookings = FileHandler.ReadBookings();
+
+            Booking selectedBooking = (Booking) (from booking in bookings
+                                              where booking.BookingID == bookingId
+                                              select booking);
+
+            if (selectedBooking == null)
+            {
+                Console.WriteLine("Enter Vaild BookingId.");
+            }
+            else
+            {
+                    Console.WriteLine(selectedBooking);
+            }
+        }
+
+
+
     }
 }
